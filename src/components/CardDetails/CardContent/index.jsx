f782fec214d2c88fdf0ab25091edc3580a2279c3
maximog <maximo.gijon@gmail.com>
2020-05-09 13:02:07 -0300

    detalles-tarjeta: creo estructura de carpetas y archivos de componentes

---
+++ src/components/CardDetails/CardContent/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function CardContext() {
+	return (
+		<div className="CardContext">
+			Contenido texto
+		</div>
+	);
+}
+
+export default CardContext;
---
+++ src/components/CardDetails/CardImage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function CardImage() {
+	return (
+		<div className="CardImage">
+			Imagen
+		</div>
+	);
+}
+
+export default CardImage;
