99bcb8bd06cccae469311be5548a48a8dc75ec96
nildemarco <nildemarco@gmail.com>
2020-05-09 15:51:51 -0300

    agregando estilo

--- src/components/Home/NavBar/index.js
+++ src/components/Home/NavBar/index.js
@@ -1,17 +1,28 @@
-import React from 'components/Home/node_modules/react';
+import React from 'react';
 import './index.scss';
-import homeheart from '../../../assets/icons/home-heart.svg';
-import heartbeat from '../../../assets/icons/heart-beat.svg';
-import salad from '../../../assets/icons/salad.svg';
-import usericon from '../../../assets/icons/user.svg';
+import { Link } from 'react-router-dom';
+import { ReactComponent as HomeHeart } from '../../../assets/icons/home-heart.svg';
+import { ReactComponent as HeartBeat } from '../../../assets/icons/heart-beat.svg';
+import { ReactComponent as Salad } from '../../../assets/icons/salad.svg';
+import { ReactComponent as UserIcon } from '../../../assets/icons/user.svg';
 
 const NavBar = () => {
     return (
         <nav className="nav-bar-container">
-            <span className="nav-bar-icons">{homeheart}</span>
-            <span className="nav-bar-icons">{salad}</span>
-            <span className="nav-bar-icons">{heartbeat}</span>
-            <span className="nav-bar-icons">{usericon}</span>
+            <button className="nav-bar-button">
+                <Link to="/">
+                    <HomeHeart className="nav-bar-icons" />
+                </Link>
+            </button>
+            <button className="nav-bar-button">
+                <Salad className="nav-bar-icons" />
+            </button>
+            <button className="nav-bar-button">
+                <HeartBeat className="nav-bar-icons" />
+            </button>
+            <button className="nav-bar-button">
+                <UserIcon className="nav-bar-icons" />
+            </button>
         </nav>
     );
 }
