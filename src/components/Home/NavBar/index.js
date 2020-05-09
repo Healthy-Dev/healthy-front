869be67a52754cb3a2c8d0d39079a976b5018d67
nildemarco <nildemarco@gmail.com>
2020-05-09 14:40:05 -0300

    agregue iconos navbar

--- src/components/Home/NavBar/index.js
+++ src/components/Home/NavBar/index.js
@@ -1,10 +1,18 @@
 import React from 'components/Home/node_modules/react';
+import './index.scss';
+import homeheart from '../../../assets/icons/home-heart.svg';
+import heartbeat from '../../../assets/icons/heart-beat.svg';
+import salad from '../../../assets/icons/salad.svg';
+import usericon from '../../../assets/icons/user.svg';
 
 const NavBar = () => {
     return (
-     <nav>
-         
-     </nav>
+        <nav className="nav-bar-container">
+            <span className="nav-bar-icons">{homeheart}</span>
+            <span className="nav-bar-icons">{salad}</span>
+            <span className="nav-bar-icons">{heartbeat}</span>
+            <span className="nav-bar-icons">{usericon}</span>
+        </nav>
     );
 }
 
