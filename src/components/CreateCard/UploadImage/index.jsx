247cafaa7200c1cff7cc5766db0a841b9cbcfa66
Maximiliano Blanco <inkcoming@protonmail.com>
2020-05-18 18:38:33 -0300

    feat(CreateCard): Revised css of UploadButton

--- src/components/CreateCard/UploadImage/index.jsx
+++ src/components/CreateCard/UploadImage/index.jsx
@@ -5,12 +5,10 @@ import "./index.scss";
 const UploadImage = () => {
 	return (
 		<div className="upload-image-container">
-				<img src="http://via.placeholder.com/80x80" alt="Example image" />
-				<div className="button-wrapper">
-					<p>Elegí una imagen de portada</p>
-					<button className="upload-image-button">Subir una imagen</button>
-					{/* <p>¡Ps! La Imagen puede pesar hasta 1Mb</p>				 */}
-				</div>
+			<img src="http://via.placeholder.com/80x80" alt="Example image" />
+			<p>Elegí una imagen de portada</p>
+			<button className="upload-image-button">Subir una imagen</button>
+			<span>¡Ps! La Imagen puede pesar hasta 1Mb</span>
 		</div>
 	);
 };
