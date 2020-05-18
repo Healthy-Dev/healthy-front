f3404f0c594de1c18e20ad7b1258a6f1730e5c40
Agus Mar <agustinam@gmail.com>
2020-05-18 12:44:54 -0300

    styled UploadImage

--- src/components/CreateCard/UploadImage/index.jsx
+++ src/components/CreateCard/UploadImage/index.jsx
@@ -5,14 +5,12 @@ import "./index.scss";
 const UploadImage = () => {
 	return (
 		<div className="upload-image-container">
-			<div className="image-wrapper">
 				<img src="http://via.placeholder.com/80x80" alt="Example image" />
-			</div>
-			<div className="button-wrapper">
-				<p>Elegí una imagen de portada</p>
-				<button>Subir una imagen</button>
-				<p>¡Ps! La Imagen puede pesar hasta 1Mb</p>
-			</div>
+				<div className="button-wrapper">
+					<p>Elegí una imagen de portada</p>
+					<button className="upload-image-button">Subir una imagen</button>
+					{/* <p>¡Ps! La Imagen puede pesar hasta 1Mb</p>				 */}
+				</div>
 		</div>
 	);
 };
