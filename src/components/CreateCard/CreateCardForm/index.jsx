c2e593034c512c8d6d2e5e26877e7949a6fe0650
Santiago Santana <santana.santiago@gmail.com>
2020-05-23 13:33:07 -0300

    Improved error msgs

--- src/components/CreateCard/CreateCardForm/index.jsx
+++ src/components/CreateCard/CreateCardForm/index.jsx
@@ -7,17 +7,23 @@ import "./index.scss";
 import AddCardButton from "components/CreateCard/AddCardButton";
 
 const CreateCardForm = () => {
-	const { register, handleSubmit, watch, errors, reset } = useForm();
+	const { register, handleSubmit, errors } = useForm();
 
-	const onSubmit = (data) => {
+	const onSubmit = async (data) => {
+		const options = {
+			method: 'POST'
+		};
+
+		await fetch("https://max-cors-anywhere.herokuapp.com/http://google.com", options);
+		console.log(data);
+
+		await fetch("https://max-cors-anywhere.herokuapp.com/http://facebook.com", options);
 		console.log(data);
 		// Add fetch function
 		// when data is submitted clear inputs
 	};
 
-	console.log(watch("title"));
-
-	const HTML_PAT = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+	const URL_FORMAT = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 	return (
 		<form className="form" onSubmit={handleSubmit(onSubmit)}>
@@ -27,7 +33,9 @@ const CreateCardForm = () => {
 				placeholder="Ingresa un título"
 				ref={register({ required: true, maxLength: 30 })}
 			/>
-			{errors.title && <p>This field is required</p>}
+			{errors.title && errors.title.type === "required" && <p>This field is required</p>}
+			{errors.title && errors.title.type === "maxLength" && <p>Max length is 30</p>}
+
 			<label>Descripción</label>
 			<input
 				name="description"
@@ -35,15 +43,19 @@ const CreateCardForm = () => {
 				ref={register({ required: true, maxLength: 200 })}
 			/>
 			{errors.description && <p>This field is required</p>}
+			{errors.description && errors.description.type === "maxLength" && (
+				<p>Max length is 200</p>
+			)}
 
 			<label>URL</label>
 			<input
 				name="url"
 				placeholder="Ingresa una URL"
-				ref={register({ required: true, maxLength: 100, pattern: HTML_PAT })}
+				ref={register({ required: true, maxLength: 254, pattern: URL_FORMAT })}
 			/>
 
 			{errors.url && errors.url.type === "required" ? <p>This field is required</p> : ""}
+			{errors.url && errors.url.type === "maxLength" ? <p>Max length is 254</p> : ""}
 			{errors.url && errors.url.type === "pattern" ? <p>This must be a URL</p> : ""}
 
 			<AddCardButton />
