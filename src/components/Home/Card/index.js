467ad40954a31b124c2af979ee2699ae9c3e6e0c
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-06 10:58:46 -0300

    Change link route to match cardDetails route

--- src/components/Home/Card/index.js
+++ src/components/Home/Card/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './index.scss';
-import { Link } from 'react-router-dom'
-import { ReactComponent as MoreVert } from './../../../assets/icons/more-vert.svg';
-import { ReactComponent as LikesHeart } from './../../../assets/icons/likes-heart.svg';
+import React from "react";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as MoreVert } from "./../../../assets/icons/more-vert.svg";
+import { ReactComponent as LikesHeart } from "./../../../assets/icons/likes-heart.svg";
 
 const Card = ({ img, title, id }) => {
-    return (
-        <div className="card__container">
-            <div className="card__likes">
-                <button className="card__likes-button">
-                    <LikesHeart className="card__likes-heart" />
-                </button>
-                <p>1.3k</p>
-            </div>
-            <img className="card__img" src={img} />
-            <div className="card__details">
-                <h2>{title}</h2>
-                <Link to="/moredetails">
-                    <MoreVert className="card__more-vert" />
-                </Link>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className="card__container">
+			<div className="card__likes">
+				<button className="card__likes-button">
+					<LikesHeart className="card__likes-heart" />
+				</button>
+				<p>1.3k</p>
+			</div>
+			<img className="card__img" src={img} />
+			<div className="card__details">
+				<h2>{title}</h2>
+				<Link to={`/details/${id}`}>
+					<MoreVert className="card__more-vert" />
+				</Link>
+			</div>
+		</div>
+	);
+};
 
 export default Card;
