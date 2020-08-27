a002146687aaf572a35a0f96110d324508e88466
Agustina Marijanovic <agustinam@gmail.com>
2020-08-27 15:25:26 -0300

    created carrousel component

---
+++ src/components/Profile/Carrousel/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./index.scss";
+import Card from "components/Home/Card"
+
+const Carrousel = () => {
+    return (
+        <div className="carrousel">
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="Correr en pista"
+                id="666" />
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="Correr "
+                id="66" />
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="Cdfdggdsta"
+                id="6" />
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="dfgista"
+                id="66656" />
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="Corrdfgda"
+                id="6646" />
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="dfga"
+                id="6656" />
+            <Card
+                img="https://www.clapps.com.ar/wp-content/uploads/2018/10/shutterstock_266758136.0.0-1068x712.jpg"
+                title="Codfgista"
+                id="6466" />
+        </div>
+    )
+}
+
+export default Carrousel;
