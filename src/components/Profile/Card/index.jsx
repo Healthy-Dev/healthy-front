f41b7aab59fb2492bcc3a9acc2269b0f9a6c8403
Agus Mar <44821883+agusmar@users.noreply.github.com>
2020-08-30 15:54:38 -0300

    Profile (#27)
    * add variables and colors in theme
    * config route for Profile view
    * add Profile view
    * created carrousel component
    * styled profile carrousel card and view, imported NavBar component, changed z-index in Navbar component located in Home mande by Nilda
    * deleted loading and error from index.js Carrousel component
    Co-authored-by: mike <myke_roly@hotmail.com>

---
+++ src/components/Profile/Card/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as LikesHeart } from "assets/icons/likes-heart.svg";
+
+const Card = ({ img, title, id }) => {
+    return (
+        <Link to={`/details/${id}`}>
+            <div className="card">
+                <img className="card__img" src={img} />
+                <div className="card__detail">
+                    <h2>{title}</h2>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default Card;
