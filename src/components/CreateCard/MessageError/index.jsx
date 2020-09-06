185fe8cf65bdd72d9e75ef77a2351a28db9c18ac
mike <myke_roly@hotmail.com>
2020-09-05 23:04:05 -0300

    deleted CreateCardForm

--- src/components/CreateCard/MessageError/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.scss";
-
-const MessageError = ({ message }) => {
-	return <p className="message__error">{message}</p>;
-};
-
-export default MessageError;
