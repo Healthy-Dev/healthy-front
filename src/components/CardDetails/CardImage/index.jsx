aa4e6a00c4d150b79038cd9a52c2348cc5f03fea
mike <myke_roly@hotmail.com>
2020-10-01 01:40:09 -0300

    counter base to likes

--- src/components/CardDetails/CardImage/index.jsx
+++ src/components/CardDetails/CardImage/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./index.scss";
-import {ReactComponent as IconLike} from "assets/icons/likes-heart.svg";
-import {ReactComponent as IconBack} from "assets/icons/arrow-left.svg";
+import { ReactComponent as IconBack } from "assets/icons/arrow-left.svg";
 import { useHistory } from "react-router-dom";
 
-const CardImage = ({ photo, title }) => {
-	const history = useHistory()
+import CountLikes from "../LikesCount";
+
+const CardImage = ({ photo, title, likesCount }) => {
+	const history = useHistory();
 	return (
 		<section className="card__header">
 			<figure className="card__header--img">
 				<img src={photo} alt={title} />
 			</figure>
 			<div className="card__header--more">
-				<button className="back" onClick={() => history.goBack()}><IconBack /></button>
-				<div className="like">
-					<IconLike className="like--icon" />
-					<span className="like--count">10</span>
-				</div>
+				<button className="back" onClick={() => history.goBack()}>
+					<IconBack />
+				</button>
+				<CountLikes likesCount={likesCount} />
 			</div>
 		</section>
 	);
-}
+};
 export default CardImage;
--- src/components/CardDetails/index.jsx
+++ src/components/CardDetails/index.jsx
@@ -18,7 +18,7 @@ const Card = ({ data, isYourCard, deleteCard }) => {
 					id={data.id}
 				/>
 			)}
-			<Image photo={data.photo} title={data.title} />
+			<Image photo={data.photo} title={data.title} likesCount={data.likesCount} />
 			<Content
 				photo={data.photo}
 				externalUrl={data.externalUrl}
