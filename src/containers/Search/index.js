75915c64bd11a310c081a4bc445a88ef5dffaaff
mike <myke_roly@hotmail.com>
2020-10-01 01:37:27 -0300

    delete route of Onboarding

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,12 +1,12 @@
 import React, { Suspense } from "react";
 import { Switch } from "react-router-dom";
 import routes from "./routes";
-import Loader from "components/_shared/Loader";
+import Loading from "components/_shared/Loading";
 // Components
 import MultipleRoutes from "components/_shared/MultipleRoutes";
 
 const Search = () => (
-	<Suspense fallback={<Loader />}>
+	<Suspense fallback={<Loading />}>
 		<Switch>
 			{routes.map((route, i) => (
 				<MultipleRoutes key={`${i}_${route.path}`} {...route} />
